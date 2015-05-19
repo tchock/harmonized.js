43c5147387eeb33eeb4565acf2a557ac5c5b35fa
Simon Jentsch <jentschs@hochschule-trier.de>
2015-05-19 16:45:30 +0200

    feat(dbhandlerFactory): add setSupportedDbHandler() function
    This function checks which db implementation is supported (IndexedbDb, WebSQL)
    and sets the corresponding dbHandler.

--- src/_dbhandler/dbhandlerfactory.js
+++ src/_dbhandler/dbhandlerfactory.js
@@ -1,7 +1,21 @@
 Harmonized.dbHandlerFactory = function dbHandlerFactory() {
+  // Set supported db handler
+  Harmonized.setSupportedDbHandler();
+};
 
+Harmonized.setSupportedDbHandler = function setSupportedDbHandler() {
+  // Set IndexedDB if supported
+  if (window.indexedDB) {
+    Harmonized.dbHandlerFactory._DbHandler = Harmonized.IndexedDbHandler;
+    // Set WebSQL if supported
+  } else if (window.openDatabase) {
+    Harmonized.dbHandlerFactory._DbHandler = Harmonized.WebSqlHandler;
+    // Set No Database if no DB support
+  } else {
+    Harmonized.dbHandlerFactory._DbHandler = undefined;
+  }
 };
 
 Harmonized.dbHandlerFactory.createDbHandler = function createDbHandler(options) {
-  
+
 };
