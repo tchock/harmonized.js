4fbb048bfd8481630989bbacc21c94c085089ea3
Simon Jentsch <sjentsch@hyphe.me>
2015-06-13 21:38:25 +0200

    refactor(serverHandler): add streams and stream handling
    Added the upstream handling (offline and online behavior).
    Added connection stream (instance and global) and behavior.

--- src/_serverhandler/_serverhandler.js
+++ src/_serverhandler/_serverhandler.js
@@ -1,13 +1,32 @@
 'use strict';
 
 Harmonized.ServerHandler = function(baseUrl, resourcePath, options) {
+  var _this = this;
+
   this._baseUrl = baseUrl;
   this._resourcePath = resourcePath;
   this._options = options;
 
   // Public streams
   this.upStream = new Rx.Subject();
+  this.upStream.subscribe(function(item) {
+    if (!_this._connected) {
+      _this._unpushedList[item.meta.rtId] = item;
+    } else {
+      _this.protocol.push(item, _this);
+    }
+  });
+
   this.downStream = new Rx.Subject();
+
+  // Instance connection stream that gets input from
+  // the global connection stream.
+  this.connectionStream = new Rx.Subject();
+  this.connectionStream.subscribe(function(state) {
+    _this.setConnectionState(state);
+  });
+
+  Harmonized.ServerHandler.connectionStream.subscribe(this.connectionStream);
 
   // List of items that couldn't be pushed to the server (e.g. because of
   // missing connection or offline mode).
@@ -32,6 +51,8 @@ Harmonized.ServerHandler = function(baseUrl, resourcePath, options) {
   this._setProtocol(useProtocol);
 };
 
+Harmonized.ServerHandler.connectionStream = new Rx.Subject();
+
 Harmonized.ServerHandler.prototype._setProtocol = function setProtocol(protocol) {
   // TODO implement protocol
   // * Check if the new protocol is available
@@ -48,6 +69,11 @@ Harmonized.ServerHandler.prototype.fetch = function fetch() {
 Harmonized.ServerHandler.prototype.pushAll = function pushAll() {
   // TODO implement pushAll
   // Has to push all items of the _unpushedList to the server resource
+};
+
+Harmonized.ServerHandler.prototype.setConnectionState = function setConnectionState(state) {
+  // TODO implement setConnectionState
+  // Set online/offline state and call pushAll if state === true
 };
 
 Harmonized.ServerHandler.prototype._createServerItem = function createServerItem(item) {
